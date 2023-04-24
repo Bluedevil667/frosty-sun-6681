ac158c7757bdcc280025157d2a1b48ae169a41e0
Logan Wilson <loganwilson777@gmail.com>
2023-04-24 17:34:35 -0600

    Refector: Move active record querying into a model method from the patients controller

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ApplicationRecord
   has_many :appointments
   has_many :doctors, through: :appointments
+
+  def self.adult_patients_in_alphabetical_order
+    where('age > ?', 18).order(name: :asc)
+  end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -3,4 +3,17 @@ require 'rails_helper'
 RSpec.describe Patient do
   it {should have_many :appointments}
   it {should have_many(:doctors).through(:appointments)}
+
+  describe 'class methods' do
+    describe '.adult_patients_in_alphabetical_order' do
+      it 'returns all patients over 18 in alphabetical order' do
+        patient_1 = Patient.create!(name: 'Katie Bryce', age: 24)
+        patient_2 = Patient.create!(name: 'Denny Duquette', age: 39)
+        patient_3 = Patient.create!(name: 'Rebecca Pope', age: 32)
+        patient_4 = Patient.create!(name: 'Zola Shepherd', age: 2)
+
+        expect(Patient.adult_patients_in_alphabetical_order).to eq([patient_2, patient_1, patient_3, ])
+      end
+    end
+  end
 end
