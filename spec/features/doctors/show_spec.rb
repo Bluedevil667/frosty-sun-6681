b6bbd87dc0b707e5d67de178d5ebb551542fec1a
Logan Wilson <loganwilson777@gmail.com>
2023-04-24 09:40:10 -0600

    User story 1

---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Doctor Show Page' do
+  before :each do
+    @hospital = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
+    @doctor = @hospital.doctors.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard University')
+    @patient_1 = @doctor.patients.create!(name: 'Katie Bryce', age: 24)
+    @patient_2 = @doctor.patients.create!(name: 'Denny Duquette', age: 39)
+    @patient_3 = @doctor.patients.create!(name: 'Rebecca Pope', age: 32)
+    @patient_4 = @doctor.patients.create!(name: 'Zola Shepherd', age: 2)
+  end
+
+  it 'shows the doctor and their attributes' do
+    visit doctor_path(@doctor)
+    
+    expect(page).to have_content(@doctor.name)
+    expect(page).to have_content(@doctor.specialty)
+    expect(page).to have_content(@doctor.university)
+  end
+
+  it 'shows the name of the hospital where the doctor works' do
+    visit doctor_path(@doctor)
+
+    expect(page).to have_content(@hospital.name)
+  end
+
+  it 'shows the names of all patients this doctor has' do
+    visit doctor_path(@doctor)
+
+    expect(page).to have_content(@patient_1.name)
+    expect(page).to have_content(@patient_2.name)
+    expect(page).to have_content(@patient_3.name)
+    expect(page).to have_content(@patient_4.name)
+  end
+end
